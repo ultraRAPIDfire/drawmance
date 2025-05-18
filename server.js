8b1a9b6453fcd686cc958b5cc78b0d2679dc43af
jc <jcreyes078@gmail.com>
2025-05-18 21:28:29 +0800

    updated server

--- server.js
+++ server.js
@@ -7,17 +7,24 @@ const io = require("socket.io")(http, {
 
 app.use(express.static("public"));
 
-io.on("connection", (socket) => {
-  console.log("A user connected");
+io.on('connection', (socket) => {
+  console.log('a user connected');
 
-  socket.on("draw", (data) => {
-    socket.broadcast.emit("draw", data); // send to all others
+  // Relay drawing events
+  socket.on('draw', (data) => {
+    socket.broadcast.emit('draw', data);
   });
 
-  socket.on("disconnect", () => {
-    console.log("A user disconnected");
+  // Relay clear event to everyone else
+  socket.on('clear', () => {
+    socket.broadcast.emit('clear');
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
   });
 });
+
 
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
